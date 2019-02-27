c7c6fc0a8a250051c019615b7d2a43df0da5f3bb
jonroby <joncaroby@gmail.com>
2019-02-26 20:14:53 -0800

    app config

---
+++ app-config.js
@@ -0,0 +1,14 @@
+const bodyParser = require("body-parser");
+
+const jsonBodyParser = bodyParser.json();
+
+const headers = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+};
+
+export { jsonBodyParser, headers };
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { jsonBodyParser, headers } = "./app-config";
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+app.use(jsonBodyParser);
+app.use(headers);
 
 app.get("/", (req, res) => res.send("Hello"));
 
