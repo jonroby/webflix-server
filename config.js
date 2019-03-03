f2a74241e265ab67be3767e591fe21a11a36d48e
jonroby <joncaroby@gmail.com>
2019-03-02 23:38:47 -0800

    use REDIS URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   PORT: process.env.PORT || 4000,
-  REDIS_PORT: process.env.REDIS_PORT || 6379,
+  REDIS_PORT: process.env.REDIS_URL || 6379,
   moviedbApiKey: process.env.api_key,
   moviedbUrl: "https://api.themoviedb.org/3"
 };
